569385ab14a7e81448b38de8275721d375b9732e
xiaoruiguo <sean.xiao@pcitc.com>
2015-11-23 22:44:06 -0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,50 +1,44 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page }  
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    
+    before { visit root_path }    
+
+    it {should have_content('Sample App')}
+    it {should have_title(full_title(''))}
+    it {should_not have_title('| Home')}
+
   end
 
   describe "Help page" do
-  
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+
+    before { visit help_path }
+     
+    it {should have_content('Help')}
+    it {should have_title('Help')}
+
   end
 
   describe "About page" do
+
+    before { visit about_path }
     
-    it "should have the content 'About Us'" do
-       visit '/static_pages/about'
-       expect(page).to have_content('About Us')
-    end
-  end
-  #Test Title
-  it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    it {should have_content('About Us')}
+    it {should have_title('About')}
+
   end
 
-  #Test Title
-  it "should have the right title 'Help'" do
-    visit '/static_pages/help'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-  end
+  describe "Contact page" do
 
-  #Test Title
-  it "should have the right title 'About'" do
-    visit '/static_pages/about'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    before { visit contact_path }
+
+    it {should have_content('Contact')}
+    it {should have_title('Contact')}
+
   end
-  
-  it "should not have a custom page title" do
-    visit '/static_pages/home'
-    expect(page).not_to have_title('| Home')
-  end  
 
 end
-
