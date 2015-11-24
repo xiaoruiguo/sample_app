58ff66fef1aeaf0aad12563280500812e3dacbdc
xiaoruiguo <sean.xiao@pcitc.com>
2015-11-23 23:18:17 -0800

    Finish CSS

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,19 @@ describe "Static pages" do
 
   subject { page }  
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title))}
+  end
+
   describe "Home page" do
     
     before { visit root_path }    
 
-    it {should have_content('Sample App')}
-    it {should have_title(full_title(''))}
+    let(:heading)    { 'Sample App'}
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it {should_not have_title('| Home')}
 
   end
@@ -17,9 +24,9 @@ describe "Static pages" do
   describe "Help page" do
 
     before { visit help_path }
-     
+    
     it {should have_content('Help')}
-    it {should have_title('Help')}
+    let(:heading) { 'Help' }    
 
   end
 
@@ -28,8 +35,7 @@ describe "Static pages" do
     before { visit about_path }
     
     it {should have_content('About Us')}
-    it {should have_title('About')}
-
+    let(:heading) { 'Help' }
   end
 
   describe "Contact page" do
@@ -37,8 +43,22 @@ describe "Static pages" do
     before { visit contact_path }
 
     it {should have_content('Contact')}
-    it {should have_title('Contact')}
-
+    let(:heading) {'Contact'}
   end
 
+  it "should have the right links on the layout" do
+     visit root_path
+
+     click_link "About"
+     expect(page).to have_title(full_title('About Us'))
+     click_link "Help"
+     expect(page).to have_title('Help') #fill in
+     click_link "Contact"
+     expect(page).to have_title('Contact') #fill in
+     click_link "Home"
+     click_link "sign up now!"
+     expect(page).to have_title('Sample') #fill in
+     click_link "sample app"
+     expect(page).to have_title('Sample') #fill in
+  end
 end
