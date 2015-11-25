77f3ce9c6666d3ec3f025a4e5fa37204d46db25f
xiaoruiguo <sean.xiao@pcitc.com>
2015-11-25 00:13:16 -0800

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ group :test do
   gem 'selenium-webdriver'
   gem 'capybara', '2.1.0'
   gem 'zip'
+  gem 'factory_girl_rails','4.2.1'
 end
 
 # Use SCSS for stylesheets
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name    "Xiao Ruiguo"
+    email   "sean.xiao@pcitc.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
