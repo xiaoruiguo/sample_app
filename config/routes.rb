3d9d7e73365fcf68610ecd26afe8e1749468cb7a
xiaoruiguo <sean.xiao@pcitc.com>
2015-12-03 00:41:03 -0800

    Finish user edit,update,index and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
   match '/signup', to: 'users#new',            via: 'get'
   match '/signin', to: 'sessions#new',         via: 'get'
-  match '/signout',to: 'sessions#destroy',     via: 'get'
+  match '/signout',to: 'sessions#destroy',     via: 'delete'
   match '/help',   to: 'static_pages#help',    via: 'get'
   match '/about',  to: 'static_pages#about',   via: 'get'
   match 'contact',to: 'static_pages#contact', via: 'get'
